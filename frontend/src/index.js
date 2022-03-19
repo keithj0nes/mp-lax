45ce5d8245679a815fd4c817e165c14510ac5e02
Keith Jones <keith@raincityambience.com>
2022-03-19 04:57:57 -0400

    trying agian

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
-import store from './redux/store'
-import { Provider } from 'react-redux'
+import store from './redux/store';
 
 
 ReactDOM.render(
@@ -15,5 +15,5 @@ ReactDOM.render(
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById('root'),
 );
